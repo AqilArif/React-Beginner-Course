2d4a7fbaf02ddb50ee34fe222bd93449daf8bacb
AqilArif <aaqil2080@gmail.com>
2023-03-10 02:26:41 +0500

    Initial Commit

---
+++ src/components/Message.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Message extends Component {
+    render() {
+        return (
+            <h1>
+                This is class component 
+            </h1>
+        )
+    }
+}
+
+export default Message;
